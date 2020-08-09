1146e3c1be3770e91e8bb9e585d723836d6cd85f
Kieu Tri Dang <kieutridang@gmail.com>
2020-08-09 11:07:59 +0700

    handle csv and dictionary

--- archieve.py
+++ archieve.py
@@ -478,3 +478,95 @@ print(set(S.lower()) >= alphabet)
 #kết quả =False vì S thiếu 2 ký tự d và g
 S = 'The quick brown fox jumps over the lazy cat'
 print(set(S.lower()) >= alphabet)
+
+# Bai 3.1.3 > 9
+
+#(i)
+from random import randint
+
+def input_n():
+    try:
+        enter_number = int(input("Please input a number between 2 and 200: "))
+        if 2 <= enter_number <= 200:
+            return enter_number
+    except:
+        print("Only integer between 2 and 200 allowed")
+
+#(ii)
+def write_file(filename, n):
+    try:
+        i = 0
+        numbers = ''
+        while i < n:
+            number = randint(-9, 200)
+            numbers += ' ' + str(number)
+            i += 1
+        with open(filename, mode = 'w+', encoding = 'utf-8') as file:
+            file.write(numbers)
+    except:
+        print("Something went wrong when writing to the file")
+    finally:
+        file.close()
+
+#(iii)
+def read_file(filename):
+    with open(filename, mode = 'r', encoding = 'utf-8') as file:
+        content = file.read()
+    return content
+
+#(iv)
+def sum(filename):
+    sum = 0
+    content = read_file(filename).strip()
+    numbers = content.split(' ')
+    for number in numbers:
+        sum += int(number)
+    return sum
+
+#(v)
+def list_out_prime_numbers(filename):
+    prime_numbers = []
+    content = read_file(filename)
+    numbers_as_string = content.strip().split(' ')
+    for number_as_string in numbers_as_string:
+        is_prime_number = int(number_as_string) > 0 and check_prime(int(number_as_string))
+        if is_prime_number:
+            prime_numbers.append(int(number_as_string))
+    return prime_numbers
+
+def check_prime(n):
+    if n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i = i + 6
+    return True
+
+n = input_n()
+write_file("./Number.txt", n)
+read_file("./Number.txt")
+
+print("sum: {}".format(sum("./Number.txt")))
+
+print("prime numbers: {}".format(list_out_prime_numbers("./Number.txt")))
+
+
+# Bai 3.2 15
+import csv
+def read_CSV_file(filename):
+    with open(filename,'r', encoding='utf8')as f:
+        csvReaderObj = csv.reader(f)
+        # Lấy dòng đầu tiên chứa các tên field ra
+        lst=csvReaderObj.__next__()
+        #In tiêu đề cột
+        print('| %21s | %20s | %10s | %13s | ' % (lst[0], lst[1], lst[2], lst[3]))
+        print('_' * 75)
+        for row in csvReaderObj:
+            print('| {: >21s}'.format(row[0]),'|', '{: <20s}'.format(row[1]), '| %10s | %13s | ' % (row[2], row[3]))
+            # print(f'Dữ liệu gồm {csvReaderObj.line_num-1}dòng')
+            
+read_CSV_file('./Departments.csv')
--- main.py
+++ main.py
@@ -1,74 +1,16 @@
-# Bai 3.1.3 > 9
-
-#(i)
-from random import randint
-
-def input_n():
-    try:
-        enter_number = int(input("Please input a number between 2 and 200: "))
-        if 2 <= enter_number <= 200:
-            return enter_number
-    except:
-        print("Only integer between 2 and 200 allowed")
-
-#(ii)
-def write_file(filename, n):
-    try:
-        i = 0
-        numbers = ''
-        while i < n:
-            number = randint(-9, 200)
-            numbers += ' ' + str(number)
-            i += 1
-        with open(filename, mode = 'w+', encoding = 'utf-8') as file:
-            file.write(numbers)
-    except:
-        print("Something went wrong when writing to the file")
-    finally:
-        file.close()
-
-#(iii)
-def read_file(filename):
-    with open(filename, mode = 'r', encoding = 'utf-8') as file:
-        content = file.read()
-    return content
-
-#(iv)
-def sum(filename):
-    sum = 0
-    content = read_file(filename).strip()
-    numbers = content.split(' ')
-    for number in numbers:
-        sum += int(number)
-    return sum
-
-#(v)
-def list_out_prime_numbers(filename):
-    prime_numbers = []
-    content = read_file(filename)
-    numbers_as_string = content.strip().split(' ')
-    for number_as_string in numbers_as_string:
-        is_prime_number = int(number_as_string) > 0 and check_prime(int(number_as_string))
-        if is_prime_number:
-            prime_numbers.append(int(number_as_string))
-    return prime_numbers
-
-def check_prime(n):
-    if n <= 3:
-        return True
-    elif n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i = i + 6
-    return True
-
-n = input_n()
-write_file("./Number.txt", n)
-read_file("./Number.txt")
-
-print("sum: {}".format(sum("./Number.txt")))
-
-print("prime numbers: {}".format(list_out_prime_numbers("./Number.txt")))
+# Cho nhập 1 chuỗi (S). Sửdụng dictionary để đếm số lần xuất hiện của từng ký tựtrong (S)
+def char_frequency(Str):
+    dict = {}
+    for C in Str:
+        keys = dict.keys()
+        if C in keys: #có trong dict => tăng value
+            dict[C] += 1
+        else: #thêm key mới là C với value=1
+            dict[C] = 1
+    return dict
+    
+Str='good morning'           
+mydict=char_frequency(Str)
+print('Các ký tự xuất hiện trong chuỗi:')
+for k,v in mydict.items():
+    print(k,'=',v,'lần')
